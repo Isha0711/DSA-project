d8331f8745c356e0f5384840e5c4fda16ec9679e
Apurba Subedi <appu.subedi@gmail.com>
2023-02-14 19:23:39 +0545

    Update actproject.c

--- actproject.c
+++ actproject.c
@@ -16,13 +16,16 @@ void create()
     char name[20];
     float a, b, c, d, e;
     float per;
+    
+	fflush(stdin);
     printf("Enter name: ");
-    scanf("%s", &name);
-    printf("Enter roll: ");
+    gets(name);
+    
+	printf("Enter roll: ");
     scanf("%d", &roll);
-    printf("Enter percentage ");
-    scanf("%f", &per);
-
+    printf("Enter marks of 5 subjects ");
+    scanf("%f%f%f%f%f", &a, &b, &e, &d, &e);
+    per = ((a + b + c + d + e) / 500) * 100;
     struct node *newNode = (struct node *)malloc(sizeof(struct node));
 
     strcpy(newNode->name, name);
@@ -53,38 +56,82 @@ void display()
     }
     else
     {
-printf("Name\tRoll No.\tPercentage\n");
+        printf("Name\t\tRoll No.\tPercentage\n");
         while (ptr != NULL)
         {
-           printf("%s\t%d\t\t%.2f%%\n", ptr->name, ptr->roll, ptr->per);
-           ptr = ptr->next;
+            printf("%s\t%d\t\t%.2f%%\n", ptr->name, ptr->roll, ptr->per);
+            ptr = ptr->next;
         }
         printf("\n");
     }
 }
 
-int main()
-{
-    int choice;
-    do
-    {
-        printf("enter 1 to create list\n");
-        printf("enter 2 to display list\n");
-        printf("enter 0 to exit list\n");
-        printf("enter choice");
-        scanf("%d", &choice);
-        switch (choice)
-        {
-        case 1:
-            create();
-            break;
-        case 2:
-            display(head);
-            break;
-            // case 0:
-            //   printf("exitted");
-            // break;
+    
+    void del_pos(){
+    	   int pos;
+    struct node *ptr;
+    int i;
+    printf("enter position:");
+    scanf("%d", &pos);
+    if (head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+
+    if (pos == 1) {
+        ptr=head;
+		head = ptr->next;
+        if (head != NULL) {
+            head->prev = NULL;
         }
-    } while (choice != 0);
+        free(ptr);
+        printf("Node at position %d deleted successfully!\n", pos);
+        return;
+    }
+    
+    for (i = 1; i < pos; i++) {
+        ptr=head;
+		ptr = ptr->next;
+    }
+    if (ptr == NULL) {
+        printf("Position %d out of range!\n", pos);
+        return;
+    }
+    ptr->prev->next = ptr->next;
+    if (ptr->next != NULL) {
+        ptr->next->prev = ptr->prev;
+    }
+    free(ptr);
+    printf("Node at position %d deleted successfully!\n", pos);
 }
 
+
+
+int main() {
+    int choice;
+    while (1) {
+        printf("\nChoose an option:\n");
+        printf("1. Add a student to the list\n");
+        printf("2. Display the list of students\n");
+        printf("3. Delete a student from the list\n");
+        printf("4. Exit the program\n");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                create();
+                break;
+            case 2:
+                display();
+                break;
+            case 3:
+              
+                del_pos();
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    }
+    return 0;
+}
